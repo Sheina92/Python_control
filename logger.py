777a32d2d8cec9a6a1d8f279934222d4f18dcf6b
Ирина <ira.sheina.92@gmail.com>
2024-02-26 16:41:46 +0300

    Добавлен вывод данных

--- logger.py
+++ logger.py
@@ -11,3 +11,21 @@ def input_data(): #Ввод данных
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{title};{text};{timestamp}\n")
         print("Заметка успешно добавлена!")
+
+def print_data(): #Вывод данных
+    with open('notes.csv', 'r', encoding='utf-8') as f:
+        data_first = f.readlines()
+    
+        j = 0
+
+        notes = []
+        for note in data_first:
+            note = note.strip()
+            title, text, timestamp = note.split(';')
+            notes.append({
+                'title': title,
+                'text': text,
+                'timestamp': timestamp
+            })
+
+        return notes

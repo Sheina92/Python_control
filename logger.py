74b48416d9c0a9e6960abb83eadfd4d67db90024
Ирина <ira.sheina.92@gmail.com>
2024-02-26 16:42:48 +0300

    Добавлено редактирование данных

--- logger.py
+++ logger.py
@@ -11,13 +11,27 @@ def input_data(): #Ввод данных
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"{title};{text};{timestamp}\n")
         print("Заметка успешно добавлена!")
-        
+
 def list_notes(): #Вывод списка заметок
     notes = print_data()
     notes.sort(key=lambda x: x['timestamp'], reverse=True)
     for i, note in enumerate(notes, 1):
         print(f"{i}. {note['title']} - {note['text']} - {note['timestamp']}")
 
+def change_data(): #Редактирование данных
+    notes = print_data()
+    list_notes()
+    note_index = int(input("Введите номер заметки, которую необходимо отредактировать: ")) - 1
+    note = notes[note_index]
+    note['title'] = input("Введите новый заголовок заметки: ")
+    note['text'] = input("Введите новую заметку: ")
+    note['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open('notes.csv', 'w', encoding='utf-8') as f:
+        text = ''.join(f'{note['title']};{note['text']};{note['timestamp']}\n' for note in notes)
+        f.write(text)
+        print("Заметка успешно отредактирована!")
+        
 def print_data(): #Вывод данных
     with open('notes.csv', 'r', encoding='utf-8') as f:
         data_first = f.readlines()
